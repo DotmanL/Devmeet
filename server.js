b61b3bdfc1201812c8356f4a9d5d1410859c4a4f
DotmanL <60046089+DotmanL@users.noreply.github.com>
2020-05-24 16:42:47 +0100

    added config folder

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
 const bodyParser = require('body-parser');
-
+const path = require('path')
 require('dotenv').config()
 
 if (process.env.NODE_ENV !== 'production') require ('dotenv').config();
@@ -12,7 +12,9 @@ connectDB();
 
 
 app.use(express.json({ extended: false }));
-app.get('/', (req, res) => res.send('API Running'));
+
+
+
 app.use(bodyParser.json());
 
 //Define Routes
@@ -21,6 +23,18 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
+
+
+
+//serve assets for production
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 5000;
 
